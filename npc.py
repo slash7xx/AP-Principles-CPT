a70200e931a567334257ecbc78a0e704298474da
slash7xx <li20291684@student.nhvweb.net>
2026-04-20 18:24:18 +0000

    npc.py

---
+++ npc.py
@@ -0,0 +1,12 @@
+attacks = []
+turn = []
+hp = 0
+enemy_condition = ["Champion", "Normal"]
+creatures = ["wolf", "mutant spider", "cyclops","Leviathan", "Yeti", "Gorgon", "Ghoul", "Minotaur"]
+classes = ["Attacker","Defender","Speedster","All-Rounder", "Technical"]
+
+#Condition to initiate fight
+
+
+
+
