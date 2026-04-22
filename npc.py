d5460b51059a9569fca5729b542c8f1e23e86f52
slash7xx <li20291684@student.nhvweb.net>
2026-04-22 15:52:23 +0000

    npc.py

--- npc.py
+++ npc.py
@@ -1,12 +1,31 @@
+import random
 attacks = []
+strength = []
+durability = []
 turn = []
-hp = 0
-enemy_condition = ["Champion", "Normal"]
-creatures = ["wolf", "mutant spider", "cyclops","Leviathan", "Yeti", "Gorgon", "Ghoul", "Minotaur"]
+hp = []
+creature = ["Wolf", "Mutant spider", "Cyclops","Leviathan", "Yeti", "Gorgon", "Ghoul", "Minotaur"]
 classes = ["Attacker","Defender","Speedster","All-Rounder", "Technical"]
-
-#Condition to initiate fight
-
-
-
-
+cre = (random.choice(creature))
+cla = (random.choice(classes))
+if classes == "Attacker":
+    hp = [10]
+    strength = [15]
+    durability = [10]
+if classes == "Defender":
+    hp = [15]
+    strength = [10]
+    durability = [15]
+if classes == "Speedster":
+    hp = [9]
+    strength = [5]
+    durability = [5]
+if classes == "All-Rounder":
+    hp = [13]
+    strength = [12]
+    durability = [12]
+if classes == "Technical":
+    hp = [11]
+    strength = [10]
+    durability  = [10]
+print(f"A {cre} approaches, it's a {cla} type")
